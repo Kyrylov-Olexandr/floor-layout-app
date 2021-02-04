424a28a253cf3d6a600ad210644e777eae987510
Kyrylov-Olexandr <59021146+Kyrylov-Olexandr@users.noreply.github.com>
2021-02-04 15:27:19 +0300

    created spring project

---
+++ src/main/java/com/floorlayoutapp/floorlayoutapp/FloorLayoutAppApplication.java
@@ -0,0 +1,13 @@
+package com.floorlayoutapp.floorlayoutapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloorLayoutAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloorLayoutAppApplication.class, args);
+	}
+
+}
